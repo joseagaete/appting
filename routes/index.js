2afb10ad99192207fc1aa4320a140f55dfd1c020
jose <joseagaete@gmail.com>
2015-11-19 19:17:51 +0000

    'version3'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var videoController = require('../controllers/videos_controller');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Lista De Reproduccion'});
+
 });
+
+
+/*Get Videos page */
+router.get('/videos', videoController.index);
+
 
 module.exports = router;
